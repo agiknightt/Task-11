065de62d84fd0fdc469d477cdd4395af1ad95261
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-08-31 22:17:56 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = 98;
+
+            for (int i = 7; i <= maxNumber; i += 7)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
